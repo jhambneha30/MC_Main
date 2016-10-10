4d4a2e273d14ad022b5ca396bc879c4191e1339e
nehaj <jhambneha30@gmail.com>
2016-10-10 19:49:04 +0530

    few screens done

---
+++ app/src/main/java/easyrent/iiitd/com/easyrent_v1/RSignUpActivity.java
@@ -0,0 +1,13 @@
+package easyrent.iiitd.com.easyrent_v1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RSignUpActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rsign_up);
+    }
+}
---
+++ app/src/main/java/easyrent/iiitd/com/easyrent_v1/RentOrPostActivity.java
@@ -0,0 +1,13 @@
+package easyrent.iiitd.com.easyrent_v1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RentOrPostActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rent_or_post);
+    }
+}
